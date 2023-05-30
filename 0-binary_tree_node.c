cda7b4ba9488fdb48a8591c4b218d4fa997f4f2b
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-05-30 17:30:17 +0000

    modified file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,7 +12,7 @@
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *Newnode = malloc(sizeof(binary_tree_t));
-	if (Newnode == NULL)
+	if (Newnode != NULL)
 	{
 		return NULL; 
 	}
