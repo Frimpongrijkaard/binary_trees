d3dbef287f11d239c94e92591d3b3a06f32c6483
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-05-31 14:52:01 +0000

    Modified file

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -19,7 +19,7 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 		return NULL; 
 	}
 	newnode-> n = value;
-	newnode-> parent = parent;
+	newnode-> parent = NULL;
 	newnode-> left = NULL;
 	newnode-> right = NULL;
 	return newnode;
