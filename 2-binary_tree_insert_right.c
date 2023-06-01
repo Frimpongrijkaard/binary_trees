398c2d0c3b3fbeed1d1b4411796ef776e5cfcaba
Frimpongrijkaard <frimpongrijkaard94@yahoo.com>
2023-06-01 20:14:39 +0000

    all files

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -11,50 +11,26 @@
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	if (parent == NULL){
+	if (parent == NULL)
+	{
 		return NULL;
 	}
 
 	binary_tree_t *new_node = malloc(sizeof(binary_tree_t));
-	if (new_node == NULL){
+	if (new_node == NULL)
+	{
 		return NULL;
 	}
 
-	new_node-> n = value;
+	new_node -> n = value;
 	new_node -> parent = parent;
 	new_node -> left = NULL;
-<<<<<<< HEAD
-	new_node -> right = parent->right;
-	parent ->right = new_node;
+	new_node -> right = parent -> right;
+	parent -> right = new_node;
 
-	if (parent-> right != NULL){
-		new_node -> right -> parent = new_node;
-	}
-
-	return new_node;
-=======
-	new_node -> right = NULL;
-
-	if (parent-> right != NULL){
-		new_node->right = parent->right;
+	if (new_node -> right != NULL)
+	{
 		parent -> right -> parent = new_node;
 	}
-
-	parent->right = new_node;
 	return new_node;
-}
-int main(void)
-{
-    binary_tree_t *root;
-
-    root = binary_tree_node(NULL, 98);
-    root->left = binary_tree_node(root, 12);
-    root->right = binary_tree_node(root, 402);
-    binary_tree_print(root);
-    printf("\n");
-    binary_tree_insert_right(root->left, 54);
-    binary_tree_insert_right(root, 128);
-    binary_tree_print(root);
-    return (0);
->>>>>>> 310b3c180f5a31992366939994594ed41c223baa
 }
